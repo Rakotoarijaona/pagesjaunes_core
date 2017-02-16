e75d842aa10298736ab053fc8a95bac7ebddb8fe
Rakotoarijaona <rakotoarijohn@gmail.com>
2017-02-16 13:25:18 +0300

    ignore temp

--- temp/www/compiled/locales/modules/ads~ads.fr_FR.UTF-8.php
+++
@@ -1,7 +0,0 @@
-<?php $_loaded= array (
-  'add.success' => 'Ajout enregisté',
-  'update.success' => 'Modification enregistée',
-  'delete.success' => 'Suppression éffectué',
-  'error' => 'Une érreur a été détectée',
-  'config.update' => 'Configuration enregistrée',
-) ?>
--- temp/www/compiled/locales/modules/entreprise~entreprise.fr_FR.UTF-8.php
+++
@@ -1,8 +0,0 @@
-<?php $_loaded= array (
-  'enregistrer.les.informations.generales' => 'Enregistrer les informations générales',
-  'enregistrer.les.informations.complementaires' => 'Enregistrer les informations complémentaires',
-  'enregistrer.les.informations.payantes' => 'Enregistrer les informations payantes',
-  'enregistrer.les.videos.et.la.galerie.payants' => 'Enregistrer les vidéos et la galerie payants',
-  'enregistrer.la.catalogue' => 'Enregistrer la catalogue',
-  'insert.informations.generales.success' => 'Entreprise enregistrée',
-) ?>
